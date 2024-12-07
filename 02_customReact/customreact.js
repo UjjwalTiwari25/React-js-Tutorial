c672a62f39bd5cbd8a69e3b41566e88e1a495b31
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-12-07 15:42:40 +0530

    notes

--- 02_customReact/customreact.js
+++ 02_customReact/customreact.js
@@ -1,15 +1,25 @@
-function customRender(reactElement, container) {
-    const domElement = document.createElement
-    (reactElement.type)
+function customRender(reactElement, container){
+    /*
+    const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement)
 }
 
 const reactElement = {
     type: 'a',
-    props: {      //props ek object hota hai
+    props: {
         href: 'https://google.com',
         target: '_blank'
     },
@@ -18,6 +28,4 @@ const reactElement = {
 
 const mainContainer = document.querySelector('#root')
 
-//ab hm kya chahte hai ki ek element ho ya method ho..jo isko render kr de...but kaise?...reactElement ko add kr de #root k andar.
-
-customRender(reactElement, mainContainer)  //but ye custom render kaam kaise karega..we can design a method
+customRender(reactElement, mainContainer)
