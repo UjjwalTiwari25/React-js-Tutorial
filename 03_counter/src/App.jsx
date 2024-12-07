01684529e6b13a85ca265beff4a33ddf3ac1a90c
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-12-07 22:11:27 +0530

    counter

--- 03_counter/src/App.jsx
+++ 03_counter/src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [counter, setCounter]  = useState(15)
+
+    //let counter = 15
+
+    const addValue = () => {
+        //counter = counter + 1
+        setCounter(prevCounter => prevCounter + 1)
+        setCounter(prevCounter => prevCounter + 1 )
+        setCounter(prevCounter => prevCounter + 1)
+        setCounter(prevCounter => prevCounter + 1)
+
+    }
+
+    const removeValue = () => {
+        setCounter(counter - 1)
+    }
+
+    return (
+        <>
+            <h1>Chai aur react</h1>
+            <h2>Counter value: {counter}</h2>
+
+            <button
+                onClick={addValue}
+            >Add value {counter}</button>
+            <br />
+            <button
+                onClick={removeValue}
+            >remove value {counter}</button>
+            <p>footer: {counter}</p>
+        </>
+    )
 }
 
 export default App
