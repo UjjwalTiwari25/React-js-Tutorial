102aeb6eb6fee19ab8fa5ecbacacabd5459dddd5
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-12-19 10:47:51 +0530

    Project 1

--- 05_ScrimbaChallenges/src/components/Header.jsx
+++ 05_ScrimbaChallenges/src/components/Header.jsx
@@ -4,11 +4,15 @@ import "../App.css";
 function Header() {
     return (
         <header>
-            <img
-                src={reactLogo}
-                alt="React logo"
-                className="react-logo"
-            />
+            <img src={reactLogo} alt="React logo" className="react-logo" />
+            <nav>
+                <ul className={"nav-list"}>
+                    <li className = "nav-list">Home</li>
+                    <li className = "nav-list">About</li>
+                    <li className = "nav-list">Contact</li>
+                </ul>
+
+            </nav>
         </header>
     );
 }
