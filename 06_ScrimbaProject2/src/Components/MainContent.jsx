87689e3a896924a16064d87569be97a36181511b
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-12-20 21:47:11 +0530

    new updates

---
+++ 06_ScrimbaProject2/src/Components/MainContent.jsx
@@ -0,0 +1,22 @@
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol className="item">
+                <li className="item">React is a popular library, so I will be able to fit in with all the coolest devs
+                    out there! 😎
+                </li>
+                <li className="item">React is a popular library, so I will be able to fit in with all the coolest devs
+                    out there! 😎
+                </li>
+                <li className="item">React is a popular library, so I will be able to fit in with all the coolest devs
+                    out there! 😎
+                </li>
+
+
+            </ol>
+        </main>
+    );
+}
+
+export default MainContent;
