62075aadf17d5289673a2258e14d3fb5637fee2d
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2025-02-12 11:06:09 +0530

    Counter App

--- 07_CounterApp/src/App.jsx
+++ 07_CounterApp/src/App.jsx
@@ -5,18 +5,29 @@ function App() {
    const [counter, setCounter] = useState(0)
   
   const addValue = () => {
-    setCounter(counter+1)
+    if (counter < 20) {
+      setCounter(counter + 1)
+    } else {
+      alert("Counter cannot exceed 20!")
+    }
   }
+
   const removeValue = () => {
-    setCounter(counter-1)
+    if (counter > 0) {
+      setCounter(counter - 1)
+    } else {
+      alert("Counter cannot go below 0!")
+    }
   }
+
   return (
     <>
-    <h1>Chai aur React</h1>
-    <h2>Counter value: {counter}</h2>
-    <button onClick={addValue}>Add Value</button>
-    <button onClick={removeValue}>Remove value</button>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
+
 export default App
